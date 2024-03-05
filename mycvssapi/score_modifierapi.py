d7948ee82fa31668b4e9b6337f00757105e85a14
Nick Leali <nleali@cisco.com>
2024-03-05 11:24:12 -0500

    added score_modifierapi

---
+++ mycvssapi/score_modifierapi.py
@@ -0,0 +1,38 @@
+# the API to return a modified vector string (and score?) based on criteria
+
+import requests
+
+# set up API router
+
+from fastapi import APIRouter
+
+router = APIRouter()
+
+from cvss4py import score_vector, vector_to_equivalence_class
+
+@router.get("/")
+def scoreMod(vectorString: str, cve: str):
+
+    # do kev check, modify string accordingly
+
+    kev_check_url = "http://127.0.0.1:8000/kev_checkapi/?search_cve=" + cve
+
+    response = requests.get(kev_check_url)
+
+    cveResponse = response.content
+    cveResponse = cveResponse.decode("utf-8")
+
+    kevState = True if cveResponse == 'true' else False
+
+    #modify vector string accordingly based on kevState
+    vectorString += '/E:A' if kevState == True else '/E:U'
+
+    #update our final vector string check
+    score_check_url = "http://127.0.0.1:8000/score_checkapi/?vector=" + vectorString
+
+    vectorResponse = requests.get(score_check_url)
+
+    modifiedScore = vectorResponse.content
+    modifiedScore = modifiedScore.decode("utf-8")
+
+    return modifiedScore
