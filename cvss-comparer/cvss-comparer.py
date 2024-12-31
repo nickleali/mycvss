e207e15314a00cb1dc2cb546416714113df05dbd
Nick Leali <nleali@cisco.com>
2024-12-30 20:36:12 -0500

    Update cvss-comparer.py
    notes, working on methods for stats, added average function for array
    
    working on options for output

--- cvss-comparer/cvss-comparer.py
+++ cvss-comparer/cvss-comparer.py
@@ -8,7 +8,16 @@ import os
 from pathlib import Path
 import objectpath
 import re
+import numpy as np
 from cvss import CVSS3, CVSS4
+
+"""
+Set the modes for output:
+
+Set outputCSV to true for CSV output.
+Set ouputAllData to true to get all the output of found vectors and scores.
+
+"""
 
 """
 Definitions for various files we're working with.
@@ -21,6 +30,7 @@ data_csv is a CSV format output of everything
 alldata_file = "data_result"
 
 csv_file = "data_csv"
+outputCSV = True
 
 data_result = open(alldata_file, 'w')
 csv_result = open(csv_file, 'w')
@@ -28,7 +38,12 @@ csv_result = open(csv_file, 'w')
 csv_result.write("CVE Name" + ", " + "CVSS v4.0 Vector String" + ", " + "CVSS v4.0 Score" + ", " + "CVSS v3 Vector String" + ", " + "CVSS v3.1 Score")
 
 # Set the list of all the files to check here
-result = list(Path("/home/kali/cvelistV5/cves").rglob("*.json"))
+result = list(Path("/home/kali/cvelistV5/cves/2024/1xxx").rglob("*.json"))
+
+# Creating our numpy array for the derived scores
+# This is a 2D array, first column v3.1, second column v4.0
+
+scoresArray = np.array([[0, 0], [1, 1]])
 
 def get_vector(vector, version):
   """Searches a string for a CVSS vector and returns the vector string only.
@@ -94,10 +109,27 @@ def get_cna(cve_file):
       except:
           pass
 
+def average_difference(arr):
+  """
+  Calculates the average between elements of a 2D array.
+
+  Args:
+    arr: A 2D NumPy array.
+
+  Returns:
+    The average between elements.
+  """
+
+  # Calculate the difference between elements and store in a new array
+  allDiffs = np.diff(arr)
+
+  # Calculate the average difference
+  avg_diff = np.mean(allDiffs)
+
+  return avg_diff
+
 # return v4.0 and then corresponding v3.1 from a file, store in an array
 # JSON format sorts in descending order, so v4.0 is always first
-
-#write CSV file headers
 
 for fname in result:
     if os.path.isfile(fname):
@@ -127,8 +159,8 @@ for fname in result:
                     # store whole line in local array
                     vector_lines.append(lines[i].strip("\n"))
 
-                    myScore = CVSS4(v4Vector)
-                    cvssv4Score = str(myScore.base_score)
+                    myv4Score = CVSS4(v4Vector)
+                    cvssv4Score = str(myv4Score.base_score)
 
                     # Write all the v4.0 stuff out.
                     cveName = str(fname)
@@ -149,8 +181,8 @@ for fname in result:
                             #print("This is the returned v3.1 vector" + v4Vector)
                        
                             # get the score of this vector
-                            myScore = CVSS3(v3Vector)
-                            cvssv3Score = str(myScore.base_score)
+                            myv3Score = CVSS3(v3Vector)
+                            cvssv3Score = str(myv3Score.base_score)
 
                             # Just in case store this in the array
                             vector_lines.append(lines[i].strip("\n"))
@@ -158,9 +190,54 @@ for fname in result:
                             # Write all the v3.1 stuff out.
                             data_result.write("CVSS v3.1 vector: " + v3Vector + "\n" + "CVSS v3.1 score: " + cvssv3Score + "\n")
                             # Write all the stuff out, this is only CVEs with both v3.1 and v4.0 scores
-                            csv_result.write(cveName + ", " + v4Vector + ", " + cvssv4Score + ", " + v3Vector + ", " + cvssv3Score + "\n")
+
+                            if outputCSV:
+                                csv_result.write(cveName + ", " + v4Vector + ", " + cvssv4Score + ", " + v3Vector + ", " + cvssv3Score + "\n")
+
+                            # Append to the numpy array
+                            newScores = np.array([[float(cvssv3Score), float(cvssv4Score)]])
+                            scoresArray = np.append(scoresArray, newScores, axis=0)
             except:
                 pass
             
         f.close()
         # print(vector_lines)
+
+        # print the output of the array just to check what we got
+
+        print("This is the output of the array:" + str(scoresArray))
+
+
+"""
+Statistical analysis of the found data
+
+Averages
+- average change
+- distance of changes
+- most common change (mode)
+
+Vulnerability classes
+- via regex, search strings for patterns
+-- Check on attack vector
+-- check on confidentiality / integrity / availability impacts
+-- command execution / RCE / ACE (as in, any CIA:High impacts)
+- do comparison by classes between v3.1 and v4.0
+
+Graph output
+- Histogram of count of changed scores (ie, how many .1 changes, how many .2 changes, etc.)
+
+How do we store the data?
+
+structured array of tuples, matching v3.1 and v4.0 scores
+- check how numpy can handle comparisons
+
+Numpy ndarray
+
+"""
+
+# Let's try some stats
+
+# Get the mean of the array
+
+print("This is the average between v3.1 and v4.0 scores for the calculated ranges:")
+print(str(average_difference(scoresArray)))
