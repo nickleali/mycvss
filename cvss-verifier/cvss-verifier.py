6a670349533f47262bf3e9563b07aa78a18276ea
Nick Leali <nleali@cisco.com>
2025-12-08 13:43:31 -0500

    Create cvss-verifier.py
    starting the new data project

---
+++ cvss-verifier/cvss-verifier.py
@@ -0,0 +1,27 @@
+## init
+
+## Description: compare the sets of reports from NVD to systematically determine the gaps in CVE descriptions
+
+## Get data from NVD
+
+### Get the entire NVD and CVE github, parse through it, and store CVSS in a database
+
+#### https://nvd.nist.gov/vuln/data-feeds
+
+## Get data out of the report as well and put that with the related record, getting the "Reason" for reference
+
+### https://nvd.nist.gov/vuln/cvmap/report/21025
+
+
+### Comparisons
+### Compare the different CVSS values in each CVE
+
+#### Compare string to examples based on CWE
+#### If there is a strong mismatch, there may be an error
+
+
+
+
+## Dashboard
+
+### Slice and search by vendor
